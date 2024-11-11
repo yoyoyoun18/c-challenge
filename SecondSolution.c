c3f904b8061c2e251f7c3d3714f9d89bfdf35f25
kimyoungjo <yoyounn88@gmail.com>
2024-11-11 17:31:48 +0900

    feat: secondsolution 완성. 앞으로 다 푼 문제들은 주석처리를 해가며 한 프로젝트 내에서 진행할 예정

---
+++ SecondSolution.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int sum = 0;
+//	int i = 0;
+//
+//	printf("1부터 n까지의 합을 구하는 프로그램입니다. 원하는 n값을 입력해보세요 ");
+//	scanf_s("%d", &i);
+//
+//	for (int j = 1; j <= i; j++) {
+//		sum += j;
+//	}
+//	printf("%d입니다.\n", sum);
+//}
